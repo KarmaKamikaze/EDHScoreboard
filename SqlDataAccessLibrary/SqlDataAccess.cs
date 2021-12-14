b3d93539f637655e37357b44735bfb59f4bfbae5
KarmaKamikaze <nicolaijgn@gmail.com>
2021-12-14 21:49:36 +0100

    Clean up SqlDataAccess

--- SqlDataAccessLibrary/SqlDataAccess.cs
+++ SqlDataAccessLibrary/SqlDataAccess.cs
@@ -5,18 +5,21 @@ using SqlDataAccessLibrary.Models;
 
 namespace SqlDataAccessLibrary;
 
-public class SqlDataAccess
+public static class SqlDataAccess
 {
-    public static List<PlayerModel> LoadPlayers()
+    public static IEnumerable<PlayerModel> LoadPlayers()
     {
         using IDbConnection connection = new SQLiteConnection("Data Source=../database.db;Version=3;");
-        var output = connection.Query<PlayerModel>("SELECT * FROM AspNetUsers", new DynamicParameters());
-        return output.ToList();
+        IEnumerable<PlayerModel>? output =
+            connection.Query<PlayerModel>("SELECT * FROM AspNetUsers", new DynamicParameters());
+        return output;
     }
-        
+    
     public static void SavePlayer(PlayerModel player)
     {
         using IDbConnection connection = new SQLiteConnection("Data Source=./database.db;Version=3;");
-        connection.Execute("INSERT INTO AspNetUsers (UserName, GamesPLayed, Points) VALUES (@PlayerTag, @GamesPlayed, @Points) ", player);
+        connection.Execute(
+            "INSERT INTO AspNetUsers (UserName, GamesPLayed, Points) VALUES (@PlayerTag, @GamesPlayed, @Points)",
+            player);
     }
 }
