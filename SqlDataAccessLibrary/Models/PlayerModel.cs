faf2966d481da7fb0a1d7ccd813e88dc3255cfa3
KarmaKamikaze <nicolaijgn@gmail.com>
2021-12-17 00:30:15 +0100

    Correct favicon, Sort PlayerModel, and add button

--- SqlDataAccessLibrary/Models/PlayerModel.cs
+++ SqlDataAccessLibrary/Models/PlayerModel.cs
@@ -1,8 +1,15 @@
 namespace SqlDataAccessLibrary.Models;
 
-public class PlayerModel
+public class PlayerModel : IComparable<PlayerModel>
 {
     public string? UserName { get; set; }
     public int GamesPlayed { get; set; }
     public int Points { get; set; }
+
+    public int CompareTo(PlayerModel? other)
+    {
+        if (ReferenceEquals(this, other)) return 0;
+        if (ReferenceEquals(null, other)) return 1;
+        return Points.CompareTo(other.Points);
+    }
 }
